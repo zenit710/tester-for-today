66d56369e5d4acb3345d85ae9f34f2b1b0a3a3c5
kmalek <kamil_malek@tvn.pl>
2021-04-22 13:34:24 +0200

    rename app

--- app.php
+++ app.php
@@ -18,29 +18,4 @@ $kernel->addParameter('gmail_user_name', getenv('GMAIL_USER_NAME') ?: '');
 $kernel->addParameter('gmail_password', getenv('GMAIL_PASS') ?: '');
 $kernel->bootstrap();
 
-//if ($argc < 2) {
-//    echo $kernel->getCommandBus()->availableCommands();
-//    exit;
-//}
-//
 echo $kernel->handle($argv[1], $argv);
-//
-//// tests
-//echo $kernel->handle('member:clear', []);
-//echo $kernel->handle('member:add', ['--name=Janusz']);
-
-/** @var \Acme\Entity\Member\MemberRepositoryInterface $memberRepo */
-//$memberRepo = $kernel->getService('MemberRepository');
-//$memberRepo->getById(2);
-
-//echo $kernel->handle('tester:clear', []);
-//echo $kernel->handle('tester:switch', ['--auto']);
-//echo $kernel->handle('tester:switch', ['--manual', '--id=1']);
-//
-//echo $kernel->handle('member:status', ['--id=1']);
-//echo $kernel->handle('member:delete', ['--id=1']);
-//
-//echo $kernel->handle('subscriber:clear', []);
-//echo $kernel->handle('subscriber:add', ['--email=kamil_malek@tvn.pl']);
-//echo $kernel->handle('subscriber:status', ['--id=1']);
-//echo $kernel->handle('subscriber:delete', ['--id=1']);
